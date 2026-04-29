66cb8d1b497bb4c13e6ef5401be862f99854ed26
chomintkhoh <chomintkhoh@gmail.com>
2026-04-29 20:48:56 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,226 @@
+window.DATA=[
+  {
+    "name": "ARGENTINA",
+    "img": "images/argentina.JPG"
+  },
+  {
+    "name": "AUSTRIA",
+    "img": "images/Austria.JPG"
+  },
+  {
+    "name": "BANGLADESH",
+    "img": "images/bangladesh.JPG"
+  },
+  {
+    "name": "BELGIUM",
+    "img": "images/Belgium.JPG"
+  },
+  {
+    "name": "BRAZIL",
+    "img": "images/brazil.JPG"
+  },
+  {
+    "name": "BRUNEI",
+    "img": "images/brunei.JPG"
+  },
+  {
+    "name": "CAMBODIA",
+    "img": "images/cambodia.JPG"
+  },
+  {
+    "name": "CANADA",
+    "img": "images/canada.JPG"
+  },
+  {
+    "name": "CHINA",
+    "img": "images/china.JPG"
+  },
+  {
+    "name": "DENMARK",
+    "img": "images/denmark.JPG"
+  },
+  {
+    "name": "ENGLAND",
+    "img": "images/england.JPG"
+  },
+  {
+    "name": "FINLAND",
+    "img": "images/finland.JPG"
+  },
+  {
+    "name": "FRANCE",
+    "img": "images/france.JPG"
+  },
+  {
+    "name": "GERMANY",
+    "img": "images/Germany.JPG"
+  },
+  {
+    "name": "INDIA",
+    "img": "images/India.JPG"
+  },
+  {
+    "name": "IRAQ",
+    "img": "images/Iraq.JPG"
+  },
+  {
+    "name": "IRELAND",
+    "img": "images/Ireland.JPG"
+  },
+  {
+    "name": "ISRAEL",
+    "img": "images/Israel.JPG"
+  },
+  {
+    "name": "ITALY",
+    "img": "images/Italy.JPG"
+  },
+  {
+    "name": "JAPAN",
+    "img": "images/Japan.JPG"
+  },
+  {
+    "name": "JOHOR",
+    "img": "images/johor.JPG"
+  },
+  {
+    "name": "KEDAH",
+    "img": "images/kedah.JPG"
+  },
+  {
+    "name": "KELANTAN",
+    "img": "images/kelantan.JPG"
+  },
+  {
+    "name": "KOREAN",
+    "img": "images/korean.JPG"
+  },
+  {
+    "name": "KUALA LUMPUR",
+    "img": "images/kuala lumpur.JPG"
+  },
+  {
+    "name": "LABUAN",
+    "img": "images/labuan.JPG"
+  },
+  {
+    "name": "LAOS",
+    "img": "images/Laos.JPG"
+  },
+  {
+    "name": "MALACA",
+    "img": "images/malaca.JPG"
+  },
+  {
+    "name": "MALAYSIA",
+    "img": "images/malaysia.JPG"
+  },
+  {
+    "name": "MALDIVES",
+    "img": "images/Maldives.JPG"
+  },
+  {
+    "name": "MEXICO",
+    "img": "images/Mexico.JPG"
+  },
+  {
+    "name": "MONGOLIA",
+    "img": "images/mongolia.JPG"
+  },
+  {
+    "name": "NEGERI SEMBILAN",
+    "img": "images/negeri sembilan.JPG"
+  },
+  {
+    "name": "NEPAL",
+    "img": "images/Nepal.JPG"
+  },
+  {
+    "name": "NEW ZEALAND",
+    "img": "images/new zealand.JPG"
+  },
+  {
+    "name": "PAHANG",
+    "img": "images/pahang.JPG"
+  },
+  {
+    "name": "PAKISTAN",
+    "img": "images/Pakistan.JPG"
+  },
+  {
+    "name": "PENANG",
+    "img": "images/penang.JPG"
+  },
+  {
+    "name": "PERAK",
+    "img": "images/perak.JPG"
+  },
+  {
+    "name": "PERLIS",
+    "img": "images/perlis.JPG"
+  },
+  {
+    "name": "PHILIPPINES",
+    "img": "images/philippines.JPG"
+  },
+  {
+    "name": "PUTRAJAYA",
+    "img": "images/putrajaya.JPG"
+  },
+  {
+    "name": "RUSSIA",
+    "img": "images/Russia.JPG"
+  },
+  {
+    "name": "SABAH",
+    "img": "images/sabah.JPG"
+  },
+  {
+    "name": "SARAWAK",
+    "img": "images/sarawak.JPG"
+  },
+  {
+    "name": "SELANGOR",
+    "img": "images/selangor.JPG"
+  },
+  {
+    "name": "SINGAPORE",
+    "img": "images/singapore.JPG"
+  },
+  {
+    "name": "SOUTH AFRICA",
+    "img": "images/south africa.JPG"
+  },
+  {
+    "name": "SPAIN",
+    "img": "images/spain.JPG"
+  },
+  {
+    "name": "SRI LANKA",
+    "img": "images/Sri lanka.JPG"
+  },
+  {
+    "name": "SWEDEN",
+    "img": "images/sweden.JPG"
+  },
+  {
+    "name": "SWITZERLAND",
+    "img": "images/Switzerland.JPG"
+  },
+  {
+    "name": "TERENGGANU",
+    "img": "images/terengganu.JPG"
+  },
+  {
+    "name": "TURKEY",
+    "img": "images/Turkey.JPG"
+  },
+  {
+    "name": "UNITED KINGDOM",
+    "img": "images/united kingdom.JPG"
+  },
+  {
+    "name": "VIETNAM",
+    "img": "images/vietnam.JPG"
+  }
+]
